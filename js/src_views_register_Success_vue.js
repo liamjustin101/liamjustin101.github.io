7e0735b122e68d041c11a3625999d3ddfa21a827
Liam Justin <liamjustin101@gmail.com>
2023-02-01 02:07:47 +0700

    Added source files.

---
+++ js/src_views_register_Success_vue.js
@@ -0,0 +1,72 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkapp_nt"] = self["webpackChunkapp_nt"] || []).push([["src_views_register_Success_vue"],{
+
+/***/ "./src/views/register/Success.vue":
+/*!****************************************!*\
+  !*** ./src/views/register/Success.vue ***!
+  \****************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Success_vue_vue_type_template_id_3c0f7911___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Success.vue?vue&type=template&id=3c0f7911& */ \"./src/views/register/Success.vue?vue&type=template&id=3c0f7911&\");\n/* harmony import */ var _Success_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Success.vue?vue&type=script&lang=js& */ \"./src/views/register/Success.vue?vue&type=script&lang=js&\");\n/* harmony import */ var _node_modules_vue_vue_loader_v15_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../../node_modules/@vue/vue-loader-v15/lib/runtime/componentNormalizer.js */ \"./node_modules/@vue/vue-loader-v15/lib/runtime/componentNormalizer.js\");\n\n\n\n\n\n/* normalize component */\n;\nvar component = (0,_node_modules_vue_vue_loader_v15_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(\n  _Success_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[\"default\"],\n  _Success_vue_vue_type_template_id_3c0f7911___WEBPACK_IMPORTED_MODULE_0__.render,\n  _Success_vue_vue_type_template_id_3c0f7911___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,\n  false,\n  null,\n  null,\n  null\n  \n)\n\n/* hot reload */\nif (false) { var api; }\ncomponent.options.__file = \"src/views/register/Success.vue\"\n/* harmony default export */ __webpack_exports__[\"default\"] = (component.exports);\n\n//# sourceURL=webpack://app-nt/./src/views/register/Success.vue?");
+
+/***/ }),
+
+/***/ "./src/views/register/Success.vue?vue&type=script&lang=js&":
+/*!*****************************************************************!*\
+  !*** ./src/views/register/Success.vue?vue&type=script&lang=js& ***!
+  \*****************************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_40_use_0_node_modules_vue_vue_loader_v15_lib_index_js_vue_loader_options_Success_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[0]!../../../node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./Success.vue?vue&type=script&lang=js& */ \"./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[0]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/views/register/Success.vue?vue&type=script&lang=js&\");\n /* harmony default export */ __webpack_exports__[\"default\"] = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_40_use_0_node_modules_vue_vue_loader_v15_lib_index_js_vue_loader_options_Success_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__[\"default\"]); \n\n//# sourceURL=webpack://app-nt/./src/views/register/Success.vue?");
+
+/***/ }),
+
+/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[0]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/views/register/Success.vue?vue&type=script&lang=js&":
+/*!*******************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[0]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/views/register/Success.vue?vue&type=script&lang=js& ***!
+  \*******************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  data() {\n    return {\n      display: false,\n      loading: true\n    };\n  },\n  mounted() {\n    document.title = 'ลงทะเบียน งาน Motor Expo 2022';\n    this.display = true;\n    this.loading = false;\n  }\n});\n\n//# sourceURL=webpack://app-nt/./src/views/register/Success.vue?./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use%5B0%5D!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options");
+
+/***/ }),
+
+/***/ "./src/views/register/Success.vue?vue&type=template&id=3c0f7911&":
+/*!***********************************************************************!*\
+  !*** ./src/views/register/Success.vue?vue&type=template&id=3c0f7911& ***!
+  \***********************************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"render\": function() { return /* reexport safe */ _node_modules_vuetify_loader_lib_loader_js_ruleSet_1_rules_0_use_node_modules_babel_loader_lib_index_js_clonedRuleSet_40_use_0_node_modules_vue_vue_loader_v15_lib_loaders_templateLoader_js_ruleSet_1_rules_4_node_modules_vue_vue_loader_v15_lib_index_js_vue_loader_options_Success_vue_vue_type_template_id_3c0f7911___WEBPACK_IMPORTED_MODULE_0__.render; },\n/* harmony export */   \"staticRenderFns\": function() { return /* reexport safe */ _node_modules_vuetify_loader_lib_loader_js_ruleSet_1_rules_0_use_node_modules_babel_loader_lib_index_js_clonedRuleSet_40_use_0_node_modules_vue_vue_loader_v15_lib_loaders_templateLoader_js_ruleSet_1_rules_4_node_modules_vue_vue_loader_v15_lib_index_js_vue_loader_options_Success_vue_vue_type_template_id_3c0f7911___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns; }\n/* harmony export */ });\n/* harmony import */ var _node_modules_vuetify_loader_lib_loader_js_ruleSet_1_rules_0_use_node_modules_babel_loader_lib_index_js_clonedRuleSet_40_use_0_node_modules_vue_vue_loader_v15_lib_loaders_templateLoader_js_ruleSet_1_rules_4_node_modules_vue_vue_loader_v15_lib_index_js_vue_loader_options_Success_vue_vue_type_template_id_3c0f7911___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vuetify-loader/lib/loader.js??ruleSet[1].rules[0].use!../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[0]!../../../node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[4]!../../../node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./Success.vue?vue&type=template&id=3c0f7911& */ \"./node_modules/vuetify-loader/lib/loader.js??ruleSet[1].rules[0].use!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[0]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[4]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/views/register/Success.vue?vue&type=template&id=3c0f7911&\");\n\n\n//# sourceURL=webpack://app-nt/./src/views/register/Success.vue?");
+
+/***/ }),
+
+/***/ "./node_modules/vuetify-loader/lib/loader.js??ruleSet[1].rules[0].use!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[0]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[4]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/views/register/Success.vue?vue&type=template&id=3c0f7911&":
+/*!************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vuetify-loader/lib/loader.js??ruleSet[1].rules[0].use!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[0]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[4]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/views/register/Success.vue?vue&type=template&id=3c0f7911& ***!
+  \************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"render\": function() { return /* binding */ render; },\n/* harmony export */   \"staticRenderFns\": function() { return /* binding */ staticRenderFns; }\n/* harmony export */ });\nvar render = function render() {\n  var _vm = this,\n    _c = _vm._self._c;\n  return !_vm.display ? _c(\"loading\", {\n    attrs: {\n      color: \"#C50030\"\n    }\n  }) : _c(\"layout\", [_vm.loading ? _c(\"loading\", {\n    attrs: {\n      color: \"#C50030\"\n    }\n  }) : _vm._e(), _c(\"base-card\", {\n    attrs: {\n      shadow: false,\n      cardFull: true\n    },\n    scopedSlots: _vm._u([{\n      key: \"header\",\n      fn: function () {\n        return [_c(\"div\", {\n          staticClass: \"titel-page\"\n        }, [_vm._v(\"ลงทะเบียน\")]), _c(\"div\", {\n          staticClass: \"titel-page\"\n        }, [_vm._v(\"งาน Motor Expo 2022\")]), _c(\"div\", {\n          staticClass: \"d-flex justify-center align-center mt-2\"\n        }, [_c(\"img\", {\n          staticClass: \"w-75\",\n          attrs: {\n            src: __webpack_require__(/*! ../../assets/images/header_img.png */ \"./src/assets/images/header_img.png\"),\n            alt: \"\"\n          }\n        })])];\n      },\n      proxy: true\n    }, {\n      key: \"body\",\n      fn: function () {\n        return [_c(\"div\", {\n          staticClass: \"d-flex justify-center mt-16\"\n        }, [_c(\"h4\", {\n          staticClass: \"thank-text\"\n        }, [_vm._v(\"ขอบคุณสำหรับการลงทะเบียน\")])])];\n      },\n      proxy: true\n    }, {\n      key: \"footer\",\n      fn: function () {\n        return undefined;\n      },\n      proxy: true\n    }])\n  })], 1);\n};\nvar staticRenderFns = [];\nrender._withStripped = true;\n\n\n//# sourceURL=webpack://app-nt/./src/views/register/Success.vue?./node_modules/vuetify-loader/lib/loader.js??ruleSet%5B1%5D.rules%5B0%5D.use!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use%5B0%5D!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet%5B1%5D.rules%5B4%5D!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options");
+
+/***/ }),
+
+/***/ "./src/assets/images/header_img.png":
+/*!******************************************!*\
+  !*** ./src/assets/images/header_img.png ***!
+  \******************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"img/header_img.cca9b4a5.png\";\n\n//# sourceURL=webpack://app-nt/./src/assets/images/header_img.png?");
+
+/***/ })
+
+}]);
