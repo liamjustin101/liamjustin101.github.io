7d5b9b814b2293dc1a4737821c054ec6f9149ef6
Liam Justin <liamjustin101@gmail.com>
2023-02-15 22:53:37 +0700

    Added fav icon.

--- aiq/nissan/api/api.js
+++ aiq/nissan/api/api.js
@@ -19,7 +19,6 @@ $('#verifyOwnershipBtn').on('click', function(event) {
 
   })
   .done(function(data) {
-    console.log(data)
     $("#responseNmt").css("text-align", "left");
     $('#responseNmt').html(JSON.stringify(data, null, 4));
     $("#spinnerNmt").hide();
@@ -40,31 +39,109 @@ $('#addNewLeadBtn').on('click', function(event) {
   $("#responseAdmin").css("text-align", "center");
   $("#spinnerAdmin").css("display", "inline-block");
 
-  var jqxhr = $.post(addNewLeadUrl,
+  var contentType = $('#contentType').find(":selected").val();
+  if(contentType == "application/x-www-form-urlencoded")
   {
-    firstName: "Liam",
-    lastName: "Justin",
-    phoneNumber: "0892223333",
-    emailAddress: "liamjustin999@gmail.com",
-    lineUid: "U847430d17de9d20a3e27d12a69b39884",
-    customerType: "prospect",
-    bu: "nmt"
-  }
-  , function() {
 
-  })
-  .done(function(data) {
-    $("#responseAdmin").css("text-align", "left");
-    $('#responseAdmin').html(JSON.stringify(data, null, 4));
-    $("#spinnerAdmin").hide();
-  })
-  .fail(function(request, status, error) {
-    $("#responseAdmin").css("text-align", "center");
-    $('#responseAdmin').html("Error");
-  })
-  .always(function() {
-     $("#spinnerAdmin").hide();
-  });
+    var jqxhr = $.post(addNewLeadUrl,
+    {
+      firstName: "Liam",
+      lastName: "Justin",
+      phoneNumber: "0892223333",
+      emailAddress: "liamjustin999@gmail.com",
+      lineUid: "U847430d17de9d20a3e27d12a69b39884",
+      customerType: "prospect",
+      bu: "nmt"
+    }
+    , function() {
+
+    })
+    .done(function(data) {
+      $("#responseAdmin").css("text-align", "left");
+      $('#responseAdmin').html(JSON.stringify(data, null, 4));
+      $("#spinnerAdmin").hide();
+    })
+    .fail(function(request, status, error) {
+      console.log(request)
+      $("#responseAdmin").css("text-align", "center");
+      $('#responseAdmin').html("Error");
+    })
+    .always(function() {
+       $("#spinnerAdmin").hide();
+    });
+  }
+  else if(contentType == "application/json")
+  {
+    $.ajax({
+      url: addNewLeadUrl,
+      type: 'POST',
+      data: 
+      JSON.stringify({
+        firstName: "Liam",
+        lastName: "Justin",
+        phoneNumber: "0892223333",
+        emailAddress: "liamjustin999@gmail.com",
+        lineUid: "U847430d17de9d20a3e27d12a69b39884",
+        customerType: "prospect",
+        bu: "nmt"
+      }),
+      contentType: contentType + '; charset=utf-8',
+      success: function (response) {
+        
+      },
+      error: function () {
+        
+      }
+    })
+    .done(function(data) {
+      $("#responseAdmin").css("text-align", "left");
+      $('#responseAdmin').html(JSON.stringify(data, null, 4));
+      $("#spinnerAdmin").hide();
+    })
+    .fail(function(request, status, error) {
+      $("#responseAdmin").css("text-align", "center");
+      $('#responseAdmin').html("Error");
+    })
+    .always(function() {
+       $("#spinnerAdmin").hide();
+    });
+  }
+  else if(contentType == "multipart/form-data")
+  {
+    var formData = new FormData();
+    formData.append('firstName', 'Liam');
+    formData.append('lastName', 'Justin');
+    formData.append('phoneNumber', '0892223333'); 
+    formData.append('emailAddress', 'liamjustin999@gmail.com')
+    formData.append('lineUid', 'U847430d17de9d20a3e27d12a69b39884')
+    formData.append('customerType', 'prospect')
+    formData.append('bu', 'nmt')
+    $.ajax({
+      url: addNewLeadUrl,
+      type: 'POST',
+      contentType: false,
+      processData: false, 
+      data: formData,
+      success: function (response) {
+        
+      },
+      error: function () {
+        
+      }
+    })
+    .done(function(data) {
+      $("#responseAdmin").css("text-align", "left");
+      $('#responseAdmin').html(JSON.stringify(data, null, 4));
+      $("#spinnerAdmin").hide();
+    })
+    .fail(function(request, status, error) {
+      $("#responseAdmin").css("text-align", "center");
+      $('#responseAdmin').html("Error");
+    })
+    .always(function() {
+       $("#spinnerAdmin").hide();
+    });
+  }
 });
 
 
